6423fcdbf6f67271733ccb97db2232dc4bec1a38
denisbanu <denisb@shoreline.io>
2023-10-18 15:52:31 +0300

    Try compile action in workflow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+env GOOS=$(echo "$OS" | tr "[:upper:]" "[:lower:]") GOARCH=amd64 go build -o dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
+
 # Select right go binary for runner os
 $GITHUB_ACTION_PATH/dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
